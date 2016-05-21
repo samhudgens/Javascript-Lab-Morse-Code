16a631e65c5cd0e6e9c7a9dd172a775f9c7c43cc
Sam Hudgens <samhudgens@Zipcoder24s-MacBook-Pro.local>
2016-05-20 21:05:18 -0400

    Add Javascript morse code translator

---
+++ MorseCode.js
@@ -0,0 +1,25 @@
+var message = prompt("Enter a message to translate to morse code");
+
+var message = message.toLowerCase();
+
+var alphabet = {
+    'a': '.-',    'b': '-...',  'c': '-.-.', 'd': '-..',
+    'e': '.',     'f': '..-.',  'g': '--.',  'h': '....',
+    'i': '..',    'j': '.---',  'k': '-.-',  'l': '.-..',
+    'm': '--',    'n': '-.',    'o': '---',  'p': '.--.',
+    'q': '--.-',  'r': '.-.',   's': '...',  't': '-',
+    'u': '..-',   'v': '...-',  'w': '.--',  'x': '-..-',
+    'y': '-.--',  'z': '--..',  ' ': '/',
+    '1': '.----', '2': '..---', '3': '...--', '4': '....-', 
+    '5': '.....', '6': '-....', '7': '--...', '8': '---..', 
+    '9': '----.', '0': '-----', 
+}
+
+var translatedMessage = "";
+
+for(var i=0; i<message.length; i++) {
+	var result = message.charAt(i);
+	translatedMessage += alphabet[result];
+}
+
+document.getElementById("result").innerHTML = translatedMessage;
